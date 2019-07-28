e63b7e93a2d4b96e8082ef88d1ac4da0efb15bf7
keng000 <4s.flashback@gmail.com>
2019-07-28 20:38:45 +0900

    update json utils

--- utils/json.go
+++ utils/json.go
@@ -8,8 +8,8 @@ import (
 	"path/filepath"
 )
 
-// Append dumps a data into a json file or append if already exists
-func Append(path string, data map[string]interface{}) error {
+// AppendWrite dumps a data into a json file or AppendWrite if already exists
+func AppendWrite(path string, data map[string]string) error {
 	if filepath.Ext(path) != ".json" {
 		return errors.New("path extension should be `.json`")
 	}
@@ -44,16 +44,23 @@ func Append(path string, data map[string]interface{}) error {
 	return nil
 }
 
-func Read(path string) (map[string]interface{}, error) {
+// Read is
+func Read(path string) (map[string]string, error) {
 	contents, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	var dump map[string]interface{}
+	var dump map[string]string
 	if err := json.Unmarshal(contents, &dump); err != nil {
 		return nil, err
 	}
 
 	return dump, nil
+}
+
+// StructToMap is
+func StructToMap(i interface{}) (map[string]string, error) {
+
+	return nil, nil
 }
